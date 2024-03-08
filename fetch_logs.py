6eedc441414d4b07f9a3b494e2346834e4367a48
Paul Menhart <paul@menhart.net>
2024-03-08 19:40:05 +0100

    add multithreaded fetch
    Create a fixed number of worker threads, that will take log_ids from a
    queue and then merge the logs afterwards.
    
    In order for this to work the streamlit.spinner, that is usually
    displayed for cached function calls needs to be disabled because
    there is a bug in the streamlit ui that sometimes throws an error
    when too many need to be displayed.

--- fetch_logs.py
+++ fetch_logs.py
@@ -1,22 +1,39 @@
+import queue
 import sys
+import threading
+from time import sleep
 
 import pandas as pd
 import requests
 import streamlit as st
+from requests.adapters import HTTPAdapter, Retry
+from streamlit.runtime.scriptrunner import add_script_run_ctx
 
 from process_logs import filter_log_data, transform_log
 
+WORKER_COUNT = 4
+# Measured on wifi with 8 cores/16 threads
+# 2 -> 67
+# 4 -> 62
+# 8 -> 59
+# 16 -> 57
+# 32 -> 62
+BASE_URL = "https://dps.report"
 
-@st.cache_data(max_entries=1000)
-def _fetch_log_data(log_id: str):
-    data_response = requests.get(f"https://dps.report/getJson?id={log_id}")
-    data_response.raise_for_status()
-    return transform_log(filter_log_data(data_response.json()), log_id)
+
+@st.cache_data(max_entries=1000, show_spinner=False)
+def _fetch_log_data(log_id: str, _session: requests.Session):
+    try:
+        data_response = _session.get(f"{BASE_URL}/getJson?id={log_id}")
+        data_response.raise_for_status()
+        return transform_log(filter_log_data(data_response.json()), log_id)
+    except Exception:
+        return pd.DataFrame()
 
 
-@st.cache_data(ttl=600)
+@st.cache_data(ttl=300)
 def fetch_log_list(userToken: str):
-    response = requests.get(f"https://dps.report/getUploads?userToken={userToken}")
+    response = requests.get(f"{BASE_URL}/getUploads?userToken={userToken}")
     response.raise_for_status()
     json = response.json()
     max_pages = json["pages"]
@@ -25,54 +42,72 @@ def fetch_log_list(userToken: str):
         if page > max_pages:
             break
         response = requests.get(
-            f"https://dps.report/getUploads?userToken={userToken}&page={page+1}"
+            f"{BASE_URL}/getUploads?userToken={userToken}&page={page+1}"
         )
         response.raise_for_status()
         uploads.extend(response.json()["uploads"])
     return [upload["id"] for upload in uploads]
 
 
-# @st.cache_data(max_entries=3)
+def _fetch_log_worker(task_queue):
+    with requests.Session() as session:
+        session.mount(
+            BASE_URL, HTTPAdapter(max_retries=Retry(total=3, backoff_factor=1))
+        )
+
+        while True:
+            try:
+                [log_buffer, index, log_id] = task_queue.get(timeout=3)
+            except queue.Empty:
+                return
+            log_buffer[index] = _fetch_log_data(log_id, session)
+            task_queue.task_done()
+
+
 def fetch_logs(log_list):
+    log_count = len(log_list)
     progress_bar = st.progress(0)
-    log_data_list = pd.DataFrame()
-    num_logs = len(log_list)
+
+    log_buffer = [pd.DataFrame()] * log_count
+    thread_list = []
+
+    # Create threads
+    task_queue = queue.Queue()
+    for _ in range(WORKER_COUNT):
+        thread = threading.Thread(
+            target=_fetch_log_worker, daemon=True, args=[task_queue]
+        )
+        thread = add_script_run_ctx(thread)
+        thread_list.append(thread)
+        thread.start()
+
+    # Distribute to workers
     for index, log_id in enumerate(log_list):
-        progress_bar.progress(index / num_logs)
-        try:
-            log_data = _fetch_log_data((log_id))
-        except Exception:
-            continue
-        log_data_list = pd.concat([log_data_list, log_data])
-
-    # XXX streamlit caching does not work with multiple threads...
-    # import threading
-    # def fetch_log_thread_func(data_list, idx, log_metadata):
-    #     data_list[idx] = fetch_log(log_metadata)
-    # Fetch all data in multiple threads
-    # data_list = [None] * len(uploads)
-    # thread_list = []
-    # for idx, log in enumerate(uploads):
-    #     thread = threading.Thread(
-    #         target=fetch_log_thread_func, args=[data_list, idx, log])
-    #     thread = add_script_run_ctx(thread)
-    #     thread_list.append(thread)
-    #     thread.start()
-    # for t in thread_list:
-    #     t.join()
-    # for data in data_list:
-    #     log_data = pd.concat([log_data, data])
-
-    log_data_list = log_data_list.sort_values("timeStart").reset_index(drop=True)
+        task_queue.put([log_buffer, index, log_id])
+    while not task_queue.empty():
+        progress_bar.progress((log_count - task_queue.qsize()) / log_count)
+        sleep(1)
     progress_bar.progress(100)
     progress_bar.empty()
-    return log_data_list
+
+    # Wait for results
+    with st.spinner(text="One more second..."):
+        task_queue.join()
+
+    # Merge buffer to a single Dataframe
+    df = pd.DataFrame()
+    for index, log in enumerate(log_buffer):
+        if not log.empty:
+            df = pd.concat([df, log])
+
+    df = df.sort_values("timeStart").reset_index(drop=True)
+    return df
 
 
 if __name__ == "__main__":
     # log_list = [fetch_log_list(sys.argv[1])[0]]
     log_id = fetch_log_list(sys.argv[1])[0]
-    data_response = requests.get(f"https://dps.report/getJson?id={log_id}")
+    data_response = requests.get(f"{BASE_URL}/getJson?id={log_id}")
     data_response.raise_for_status()
     d = data_response.json()
     import json
