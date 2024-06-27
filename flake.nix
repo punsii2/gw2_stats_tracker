06342b614c508d93277e75d013a823a6fb3f396f
Paul Menhart <paul@menhart.net>
2024-06-27 15:19:34 +0200

    add treefmt-nix

--- flake.nix
+++ flake.nix
@@ -1,10 +1,27 @@
 {
   description = "statistics tracker aimed at gw2 gvgs";
 
-  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-23.05";
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    treefmt-nix.url = "github:numtide/treefmt-nix";
+  };
 
-  outputs = { self, nixpkgs }:
+  outputs = { self, nixpkgs, treefmt-nix }:
+
     let
+      treefmtEval = treefmt-nix.lib.evalModule pkgs
+        {
+          # Used to find the project root
+          projectRootFile = "flake.nix";
+
+          programs = {
+            black.enable = true;
+            isort.enable = true;
+            prettier.enable = true;
+            nixpkgs-fmt.enable = true;
+          };
+        };
+
       pkgs = import nixpkgs {
         system = "x86_64-linux";
       };
@@ -14,9 +31,13 @@
       ];
     in
     {
+      formatter.x86_64-linux = treefmtEval.config.build.wrapper;
+
       devShells.x86_64-linux = {
         default = pkgs.mkShell {
           buildInputs = with pkgs; [
+            treefmtEval.config.build.wrapper
+
             (python3.withPackages pyPkgs)
             streamlit
 
